d105207afd9036fb23073248e0991c688138fa62
zhoujing <zhoujing204@outlook.com>
2023-11-21 11:05:29 +0800

    update lecture 7 open course.

---
+++ src/15-python-web-apps/starting_a_new_project/learning_logs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Topic
+
+
+admin.site.register(Topic)
---
+++ src/15-python-web-apps/starting_a_new_project/learning_logs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
